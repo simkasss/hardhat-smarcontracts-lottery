ce95785e9a25cf02415441531f895eefebf7aaf6
simkasss <s.kastantinaviciute@gmail.com>
2023-01-27 14:52:54 +0200

    TODO4 V2

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -4,7 +4,7 @@ const networkConfig = {
   5: {
     name: "goerli",
     vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
-    entranceFee: ethers.utils.parseEther("0.01"),
+    entranceFee: ethers.utils.parseEther("10"),
     gasLane:
       "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
     subscriptionId: "8550",
@@ -14,7 +14,7 @@ const networkConfig = {
   31337: {
     name: "hardhat",
     subscriptionId: "558",
-    entranceFee: ethers.utils.parseEther("0.01"),
+    entranceFee: ethers.utils.parseEther("10"),
     gasLane:
       "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
     callbackGasLimit: "500000",
